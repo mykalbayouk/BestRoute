fa55d58908b29b0dfc76316f8a5046bfe2f0fefc
Michael Bayouk <mb49491@uga.edu>
2023-02-04 13:23:36 -0500

    Have getTime and getDistance for travelAPI

--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/processors/TravelDistanceProcessor.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/processors/TravelDistanceProcessor.java
@@ -11,11 +11,14 @@ import okhttp3.Response;
 public class TravelDistanceProcessor implements Processor {
     //This class will process the travel distance data from an API.
     //Make a constructor that will take in all the data this class needs to process.
+
+    private String jsonString;
+
     public void process(String origin, String destination) {
         getJSON(origin, destination);
     }
 
-    public String getJSON(String origin, String destination) {
+    public void getJSON(String origin, String destination) {
         try {
             String urlC = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + origin + "&destinations=" + destination;
             String apiKey = "&key=AIzaSyCF53H3HxflG8kBxxLGHBH4sP_pcEwmNqI";
@@ -28,9 +31,20 @@ public class TravelDistanceProcessor implements Processor {
                 .method("GET", body)
                 .build();
             Response response = client.newCall(request).execute();
-            return response.body().string();
+            jsonString = response.body().string();
         } catch (IOException e) {
-            return "Error: " + e;     
+            System.out.println("Invalid Input");     
         }
     }
+
+    public String getDistance(String origin, String destination) {
+        return jsonString.substring(jsonString.indexOf("distance") + 11, jsonString.indexOf("duration") - 3); 
+        
+    }
+
+    public String getTime(String origin, String destination) {
+        return jsonString.substring(jsonString.indexOf("duration") + 11, jsonString.indexOf("duration") + 20);
+        
+    }
+
 }
