f9d03dd7c7bd7655bce866f7b151e14bd0b4e0cb
George Herrmann <gvhmann@gmail.com>
2023-02-04 13:04:26 -0500

    small tweaks

--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/BestRouteApplication.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/BestRouteApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BestRouteApplication {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args)  {
 		SpringApplication.run(BestRouteApplication.class, args);
 	}
 
