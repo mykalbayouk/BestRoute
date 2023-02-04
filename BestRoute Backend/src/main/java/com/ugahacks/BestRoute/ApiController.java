46fa426f7033c4436a77d2afc48c84a145927b3e
George Herrmann <gvhmann@gmail.com>
2023-02-04 12:36:12 -0500

    working on api controller mapping

--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/ApiController.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/ApiController.java
@@ -3,7 +3,7 @@ package com.ugahacks.BestRoute;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ugahacks.BestRoute.data.ExportableUserData;
@@ -21,7 +21,8 @@ public class ApiController {
 
     //Will receive data from the frontend
     @PostMapping("/send")
-    public void receive(@RequestBody ImportedUserData data) {
-        throw new UnsupportedOperationException(); //temp
+    public String receive(@RequestParam ImportedUserData data) {
+        System.out.println(data.getCarMake());
+        return "Received";
     }
 }
--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/Manager.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/Manager.java
@@ -44,7 +44,4 @@ public class Manager {
         return exportableUserData;
     }
 
-
-
-
 }
