7b8433996a44244da20ad10d1455ea2775ffd0f5
Michael Bayouk <mb49491@uga.edu>
2023-02-04 12:23:48 -0500

    adding milespergallon functionality

--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/Manager.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/Manager.java
@@ -8,15 +8,6 @@ import com.ugahacks.BestRoute.processors.MilePerGallonProcessor;
 import com.ugahacks.BestRoute.processors.TravelDistanceProcessor;
 
 public class Manager {
-    //Rough starter code for API calls
-    /*URL url = new URL("https://api.api-ninjas.com/v1/cars?model=camry");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("accept", "application/json");
-            connection.addRequestProperty("X-Api-Key", "4UiF3NQnApBFWB8X29ET3FAvJnM1NHjXNzihJ6K0");
-            InputStream responseStream = connection.getInputStream();
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(responseStream);
-            String s = root.findValuesAsText("combination_mpg").get(0);*/
 
     private final ImportedUserData data;
     private ExportableUserData exportableUserData;
@@ -32,6 +23,8 @@ public class Manager {
         gasPriceProcessor = new GasPriceProcessor();
         travelDistanceProcessor = new TravelDistanceProcessor();
     }
+
+
 
     protected void process() {
         flightCostProcessor.process();
--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/processors/MilePerGallonProcessor.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/processors/MilePerGallonProcessor.java
@@ -1,9 +1,37 @@
 package com.ugahacks.BestRoute.processors;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class MilePerGallonProcessor implements Processor {
     //This class will process the mile per gallon data from an API.
     //Make a constructor that will take in all the data this class needs to process.
-    public void process() {
-        throw new UnsupportedOperationException();
+    private String api_MPG;
+
+
+
+    public void process(String make, String model, String year) {
+        getAPI(make, model. year);
+    }
+
+    public void getAPI(String make, String model, String year) {
+        try { 
+            URL url = new URL("https://api.api-ninjas.com/v1/cars?model=camry");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("accept", "application/json");
+            connection.addRequestProperty("X-Api-Key", "4UiF3NQnApBFWB8X29ET3FAvJnM1NHjXNzihJ6K0");
+            InputStream responseStream = connection.getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(responseStream);
+            api_MPG = root.findValuesAsText("combination_mpg").get(0);
+        } catch (IOException e) {
+            e.printStackTrace(); 
+        } // try catch
+        
     }
 }
