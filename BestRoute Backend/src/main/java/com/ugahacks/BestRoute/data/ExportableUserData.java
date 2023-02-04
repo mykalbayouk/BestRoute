2a5d7506aabed4a5029dfed903caf7fbbf637444
Jackson Greer <jlg40585@uga.edu>
2023-02-04 13:08:52 -0500

    did export file

--- BestRoute Backend/src/main/java/com/ugahacks/BestRoute/data/ExportableUserData.java
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/data/ExportableUserData.java
@@ -3,4 +3,32 @@ package com.ugahacks.BestRoute.data;
 public class ExportableUserData {
     //Should contain strings, ints, etc. Basically all the deconstructed data from the backend.
     //This is the data that will be sent to the frontend to be displayed after the manager has processed the data.
+
+    private String driveCost;
+    private String driveTime;
+    private String flightCost;
+    private String flightTime;
+
+    public ExportableUserData(String driveCost, String driveTime, String flightCost, String flightTime) {
+        this.driveCost = driveCost;
+        this.driveTime = driveTime;
+        this.flightCost = flightCost;
+        this.flightTime = flightTime;
+    } // ExportableUserData
+
+    public String getDriveCost() {
+        return driveCost;
+    } // getDriveCost
+
+    public String getDriveTime() {
+        return driveTime;
+    } // getDriveTime
+
+    public String getFlightCost() {
+        return flightCost;
+    } // getFlightCost
+
+    public String getFlightTime() {
+        return flightTime;
+    } // getFlightTime
 }
