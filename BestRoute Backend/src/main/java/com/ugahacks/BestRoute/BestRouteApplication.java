88d900e9f5a71929c53c21cbe110851403478f72
George Herrmann <gvhmann@gmail.com>
2023-02-03 21:12:09 -0500

    added spring backend

---
+++ BestRoute Backend/src/main/java/com/ugahacks/BestRoute/BestRouteApplication.java
@@ -0,0 +1,13 @@
+package com.ugahacks.BestRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestRouteApplication.class, args);
+	}
+
+}
---
+++ BestRoute Backend/src/test/java/com/ugahacks/BestRoute/BestRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugahacks.BestRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
